1174555cf36346a7475c1b1d1fb1309c78622400
GurumoorthysHub <gurumoorthy.s2004@gmail.com>
2025-08-07 21:26:58 +0530

    added

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.25.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx"
+  image = docker_image.nginx.image_id
+
+  ports {
+    internal = 80
+    external = 1030
+  }
+}
